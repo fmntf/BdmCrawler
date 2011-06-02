71ff0a891fd6dad677b4ecc9c54a113b1b2c23cc
Francesco Montefoschi <francesco.monte@gmail.com>
2011-06-02 18:47:42 +0200

    added license

--- src/it/unisi/bdm/crawler/HtmlAnalyzer.java
+++ src/it/unisi/bdm/crawler/HtmlAnalyzer.java
@@ -1,7 +1,23 @@
+/**
+ * This file is part of BdmCrawler.
+ * BdmCrawler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BdmCrawler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BdmCrawler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.unisi.bdm.crawler;
 
 import org.w3c.tidy.Tidy;
-import org.w3c.tidy.*;
+import org.w3c.tidy.DOMElementImpl;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import java.io.Reader;
@@ -13,20 +29,23 @@ import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.OutputKeys;
 import javax.xml.xpath.XPathFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 
 /**
- *
- * @author francesco
+ * HtmlAnalyzer
+ * 
+ * Tidies the given HTML markup and extracts the link tags (<a href=""></a>)
  */
 public class HtmlAnalyzer
 {
 	private Document page;
 	
+	/**
+	 * @param String html 
+	 */
 	public HtmlAnalyzer(String html)
 	{
 		Tidy tidy = new Tidy();
@@ -38,6 +57,11 @@ public class HtmlAnalyzer
 		this.page = tidy.parseDOM(input, null);
 	}
 	
+	/**
+	 * Extracts a list of links found in the <body> of the document.
+	 * 
+	 * @return ArrayList<HtmlLink>
+	 */
 	public ArrayList<HtmlLink> getLinks()
 	{
 		ArrayList list = new ArrayList<HtmlLink>();
