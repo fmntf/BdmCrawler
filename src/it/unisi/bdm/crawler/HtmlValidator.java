27a43e40a186a6b8ee5a15900fcc940c23e72549
Francesco Montefoschi <francesco.monte@gmail.com>
2011-06-25 09:53:49 +0200

    validation of HTML pages

---
+++ src/it/unisi/bdm/crawler/HtmlValidator.java
@@ -0,0 +1,50 @@
+/**
+ * This file is part of BdmCrawler.
+ * BdmCrawler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BdmCrawler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BdmCrawler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.unisi.bdm.crawler;
+
+import org.w3c.tidy.Tidy;
+import org.w3c.tidy.TidyMessageListener;
+import java.io.ByteArrayInputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.PrintWriter;
+
+public class HtmlValidator
+{
+	public int tidy(String html)
+	{
+		Tidy tidy = new Tidy();
+
+		// suppress ugly output
+		PrintWriter errorWriter = new PrintWriter(new StringWriter(), true);
+		tidy.setErrout(errorWriter);
+		
+		tidy.parse(this.stringToStream(html), new StringWriter());
+		
+		return 
+				tidy.getParseWarnings() +
+			2 * tidy.getParseErrors();
+	}
+	
+	private InputStreamReader stringToStream(String string)
+	{
+		byte[] bytes = string.getBytes();
+		ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+		
+		return new InputStreamReader(bais);
+	}
+}
---
+++ test/it/unisi/bdm/crawler/HtmlValidatorTest.java
@@ -0,0 +1,62 @@
+/**
+ * This file is part of BdmCrawler.
+ * BdmCrawler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * BdmCrawler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with BdmCrawler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.unisi.bdm.crawler;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HtmlValidatorTest
+{
+	@Test
+	public void wontReportErrorsOnValidPage()
+	{
+		HtmlValidator validator = new HtmlValidator();
+		
+		int mess = validator.tidy(this.readFile("test/fixtures/html/w3.org.html"));
+		assertEquals(0, mess);
+	}
+	
+	@Test
+	public void repostsErrorsOnInvalidPage()
+	{
+		HtmlValidator validator = new HtmlValidator();
+		
+		int mess = validator.tidy(this.readFile("test/fixtures/html/iveco.com.html"));
+		assertEquals(12, mess);
+	}
+	
+	private String readFile( String file )
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		try {
+			BufferedReader reader = new BufferedReader( new FileReader (file));
+			String line  = null;
+			while( ( line = reader.readLine() ) != null ) {
+				stringBuilder.append(line).append("\n");
+			}
+		}
+		catch (IOException e) {
+			fail(e.getMessage());
+		}
+		
+		return stringBuilder.toString();
+	}
+}
